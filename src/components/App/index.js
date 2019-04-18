c22a884f1ebbeb3bb93baa013e40e3d6b38193a5
marcin <makepeopleup@gmail.com>
2019-04-17 21:20:05 -0500

    initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './styles.css'
+import Homepage from '../Homepage'
+import Login from '../Login'
+import Register from '../Register'
+import Dashboard from '../Dashboard'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { CssBaseline, CircularProgress } from '@material-ui/core'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import firebase from '../firebase'
+
+export default function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Homepage} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />            
+                <Route exact path="/dashboard" component={Dashboard} />
+            </Switch>
+        </Router>
+    )    
+}
+
---
+++ src/components/Homepage/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from "antd"
+
+const styles = theme => ({})
+
+function Hompage(props) {
+    const { classes } = props
+
+    return (
+        <div>
+            <div>
+                <h1>
+                    homepage.js
+                </h1>
+            </div>
+            <h2>
+                Hello Guest
+            </h2>
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="secondary"
+                component={Link}
+                to="/register"
+                className={classes.submit}>
+            Register
+            </Button>
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="secondary"
+                component={Link}
+                to="/login"
+                className={classes.submit}>
+            Login
+            </Button>
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="secondary"
+                component={Link}
+                to="/dashboard"
+                className={classes.submit}>
+            Dashboard
+            </Button>
+        </div>
+    )
+}
+
+export default Homepage
